0afbf8e643b9b60d1337ff07ac71a801250f3ba8
Lucas Savva <lucas@m1cr0man.com>
2018-05-16 22:14:47 +0100

    Enable Swagger support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { M1cr0blogModule } from './modules/m1cr0blog/module'
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(M1cr0blogModule)
+
+    // Set up Swagger
+    const options = new DocumentBuilder()
+        .setTitle('M1cr0blog API')
+        .setDescription('Backend REST API for m1cr0man\'s blog')
+        .setVersion('1.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('api', app, document)
 
     await app.listen(3000)
 }
