216093bc7a81c1495249b3a20faf6654aa446419
Lucas Savva <lucas@m1cr0man.com>
2018-06-21 11:37:09 +0100

    Add user management pages
    TODO POST endpoint for user edit

--- src/modules/m1cr0blog/controller.ts
+++ src/modules/m1cr0blog/controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Inject, Render } from '@nestjs/common'
+import { Controller, Get, Inject, Param, Render, UseGuards } from '@nestjs/common'
 import { Blog, BlogsService } from '../blogs'
+import { AuthGuard, UserService } from '../users'
 
 
 const TEMPLATE_DATA = {
     titleSuffix: 'M1cr0man\'s blog',
     title: 'M1cr0man\'s blog',
-    mainClasses: '',
+    mainClasses: 'pure-g',
     meta: {
         url: '/'
     }
+}
+
+const TEMPLATE_DATA_ADMIN = {
+    ...TEMPLATE_DATA,
+    mainClasses: 'pure-g padded-body'
 }
 
 const EMPTY_BLOG = new Blog('-1', 'empty', 'No posts yet', new Date(), 'Come back soon!')
@@ -17,14 +23,44 @@ const EMPTY_BLOG = new Blog('-1', 'empty', 'No posts yet', new Date(), 'Come bac
 export class M1cr0blogController {
     constructor(
         @Inject(BlogsService)
-        private readonly blogsServie: BlogsService
+        private readonly blogsService: BlogsService,
+        @Inject(UserService)
+        private readonly usersService: UserService
     ) {
     }
 
     @Get()
     @Render('index')
     root() {
-        const blog = this.blogsServie.findLatest() || EMPTY_BLOG
+        const blog = this.blogsService.findLatest() || EMPTY_BLOG
         return {...TEMPLATE_DATA, blog}
+    }
+
+    // TODO admin endpoints
+    // TODO blogs PUT request
+    // TODO template locals
+    @Get('/admin/')
+    @UseGuards(AuthGuard)
+    @Render('admin/index')
+    adminHome() {
+        return TEMPLATE_DATA_ADMIN
+    }
+
+    @Get('/admin/users/')
+    @UseGuards(AuthGuard)
+    @Render('admin/users/index')
+    adminUsersHome() {
+        const users = this.usersService.find()
+        return {...TEMPLATE_DATA_ADMIN, users}
+    }
+
+    @Get('/admin/users/:id/')
+    @UseGuards(AuthGuard)
+    @Render('admin/users/edit')
+    adminUsersEdit(
+        @Param('id') id: string
+    ) {
+        const user = this.usersService.findOne(id)
+        return {...TEMPLATE_DATA_ADMIN, user}
     }
 }
