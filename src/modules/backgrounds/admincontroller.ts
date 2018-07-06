3510427bf9dc43b15cb3bee17673a5a4f29d210d
Lucas Savva <lucas@m1cr0man.com>
2018-07-06 19:45:33 +0100

    Add base of backgrounds admin controller

---
+++ src/modules/backgrounds/admincontroller.ts
@@ -0,0 +1,50 @@
+import { Controller, Get, Inject, Param, Query, Render, UseGuards } from '@nestjs/common'
+import { ApiUseTags } from '@nestjs/swagger'
+import { TEMPLATE_DATA_ADMIN } from '../../constants'
+import { AuthGuard, UserService } from '../users'
+import { BackgroundsService } from './service'
+
+
+@ApiUseTags('Backgrounds Admin')
+@UseGuards(AuthGuard)
+@Controller('/admin/users/:userId/backgrounds')
+export class BackgroundsAdminController {
+    constructor(
+        @Inject(UserService)
+        private readonly service: UserService
+    ) {
+    }
+
+    getService(userId: string): BackgroundsService {
+        return new BackgroundsService(this.service.findOne(userId).backgrounds)
+    }
+
+    @Get()
+    @Render('admin/backgrounds/index')
+    adminBackgroundsHome(
+        @Param('userId') userId: string
+    ) {
+        const ents = this.getService(userId).find()
+        const binned_tags = ents.reduce((group, ent) => {
+            for (let tag of ent.tags) {
+                group[tag] = (group[tag] || 0) + 1
+            }
+            return group
+        }, {})
+        const tags = Object.keys(binned_tags).map(t => ({name: t, images: binned_tags[t]}))
+        return {...TEMPLATE_DATA_ADMIN, tags}
+    }
+
+    @Get(':tag/')
+    @Render('admin/backgrounds/edit')
+    adminBackgroundsEdit(
+        @Param('userId') userId: string,
+        @Param('tag') tag: string,
+        @Query('page') page?: string
+    ) {
+        const intpage = (page && +page) || 0
+        const query = this.getService(userId).find().filter(x => x.tags.indexOf(tag) + 1)
+        const images = query.slice(intpage * 20, (intpage + 1) * 20)
+        return {...TEMPLATE_DATA_ADMIN, userId, tag, images}
+    }
+}
--- src/modules/backgrounds/module.ts
+++ src/modules/backgrounds/module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from '../users'
+import { BackgroundsAdminController } from './admincontroller'
 import { BackgroundsController } from './controller'
 
+
 @Module({
-    controllers: [BackgroundsController],
+    controllers: [BackgroundsController, BackgroundsAdminController],
     imports: [UserModule]
 })
 export class BackgroundsModule {
